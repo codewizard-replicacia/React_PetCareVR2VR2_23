bc63f4e6fb2dc6b9830202dd746babac14dedd1e
Administrator <Administrator@172.31.36.201>
2023-09-16 04:12:05 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,41 @@
+export const petServiceViewConfig = {
+Details: [
+    {
+      key: "ServiceType",
+      value: "ServiceType",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Price",
+      value: "Price",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "DurationInDays",
+      value: "DurationInDays",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ServiceAt",
+      value: "ServiceAt",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "DateOfService",
+      value: "DateOfService",
+      type: "date",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
